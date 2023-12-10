ce24ad45770aea69fbf4a16e7a39072989bfbf14
vaibhav21898 <vj2598054@gmail.com>
2023-12-10 13:34:31 +0530

    ui update

--- src/components/Travel/Populardestinations.tsx
+++ src/components/Travel/Populardestinations.tsx
@@ -1,8 +1,23 @@
 const Populardestinations = () => {
+  // fetch(
+
+  // );
+
   return <div>Populardestinations</div>;
 };
+export async function getStaticProps() {
+  // Fetch data from an API or any data source
+  const response = await fetch(
+    `https://travelimpactmodel.googleapis.com/v1/flights:computeFlightEmissions?key=AIzaSyB_u5cDhQgCxTadFs8cvwkbhWEC-YsbjPU`
+  );
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
 
 export default Populardestinations;
-
-// https://travelimpactmodel.googleapis.com/v1/flights:computeFlightEmissions?key=$API_KEY
-// AIzaSyB_u5cDhQgCxTadFs8cvwkbhWEC-YsbjPU
