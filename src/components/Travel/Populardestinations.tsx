13c82edc021100a624cb7031d010102b809fe1e9
vaibhav21898 <vj2598054@gmail.com>
2023-12-18 00:21:31 +0530

    working on  map

--- src/components/Travel/Populardestinations.tsx
+++ src/components/Travel/Populardestinations.tsx
@@ -1,6 +1,44 @@
+import GoogleMapReact from "google-map-react";
 import ICONS from "@/assets/icons";
 import { travelData } from "../core/Data";
+import { useState } from "react";
+interface MarkerProps {
+  lat: number;
+  lng: number;
+  text: string;
+  selected: boolean;
+  onClick: () => void;
+}
+
+const Marker: React.FC<MarkerProps> = ({ text, selected, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={` h-6 w-6 border-2 border-white bg-sky-500 rounded-full  ${
+        selected ? " " : ""
+      }`}
+    >
+      {/* {text} */}
+    </div>
+  );
+};
 const Populardestinations = () => {
+  const indiaCenter = { lat: 20.5937, lng: 78.9629 }; // Center of India
+  const indiaZoom = 4.5; // Zoom level for India
+  const [selectedMarker, setSelectedMarker] = useState<number | null>(null);
+  const markers = [
+    { lat: 28.6139, lng: 77.209, text: "Delhi" },
+    { lat: 12.9716, lng: 77.5946, text: "Bengaluru" },
+    { lat: 19.076, lng: 72.8777, text: "Mumbai" },
+  ];
+
+  // const mapOptionsTerrain: any = {
+  //   mapTypeId: "terrain",
+  //   zoomControlOptions: {
+  //     position: window.google.maps.ControlPosition.RIGHT_TOP,
+  //   },
+  // };
+
   return (
     <div className="flex main-container md:flex-row flex-col gap-4">
       <div className="w-full flex flex-col gap-y-3 ">
@@ -39,7 +77,32 @@ const Populardestinations = () => {
           </div>
         ))}
       </div>
-      <div className="w-full border-2 border-green-600"></div>
+      <div className="w-full ">
+        <div
+          className="rounded-2xl"
+          style={{ height: "100%", width: "100%", borderRadius: "100px" }}
+        >
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyB_u5cDhQgCxTadFs8cvwkbhWEC-YsbjPU",
+            }}
+            defaultCenter={indiaCenter}
+            defaultZoom={indiaZoom}
+          >
+            {markers.map((marker, index) => (
+              <Marker
+                key={index}
+                lat={marker.lat}
+                lng={marker.lng}
+                text={marker.text}
+                selected={index === selectedMarker}
+                onClick={() => setSelectedMarker(index)}
+                // options={mapOptionsTerrain}
+              />
+            ))}
+          </GoogleMapReact>
+        </div>
+      </div>
     </div>
   );
 };
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,4 @@
 // declare module 'react-date-range'
+declare module '@types/google-map-react'
 // declare module 'file-saver'
 // *********************************** any library gives type error then you can add that library type here********************************************
